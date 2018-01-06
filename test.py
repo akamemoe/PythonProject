c39c760a3a5e1f48e22e4727d78cdb884a62e123
AmyDestiny <1242080075@qq.com>
2018-01-07 00:03:06 +0800

    test travic-ci

---
+++ test.py
@@ -0,0 +1,14 @@
+# coding:utf-8
+
+import requests
+import re
+
+def main():
+	r = requests.get('https://www.baidu.com')
+	print(r.status_code)
+	print(r.content.decode('utf-8'))
+	title = re.findall(r'<title>(.*)</title>',r.content.decode('utf-8'))
+	print('title:',title)
+
+if __name__ == '__main__':
+	main()
