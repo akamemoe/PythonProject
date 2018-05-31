199568b46cbd83dd42804f8f7ab011c71a6c69e8
AmyDestiny <1242080075@qq.com>
2018-06-01 01:40:27 +0800

    update room info regex

--- src/huya/huya_anchor.py
+++ src/huya/huya_anchor.py
@@ -10,8 +10,8 @@ huya_url = 'http://www.huya.com/'
 
 def send_msg(text='title',desp='desp'):
     notify_url = 'https://sc.ftqq.com/' + os.getenv('scu_key') +'.send'
-    r = requests.post(notify_url,data={'text':text,'desp':desp})
-    return True if r and r.status_code == 200 else False
+    r = requests.post(notify_url,data={'text':text,'desp':desp},timeout=5)
+    return r and r.ok and r.json()['errmsg'] == 'success'
 
 def fetch(file_path):
     with open(file_path,'r',encoding='utf-8') as f:
@@ -23,15 +23,15 @@ def fetch(file_path):
             anchors.append((a,b))
     txt = '| 主播 | 标题 | 状态 | 订阅 |\n|:---:|:---:|:---:|:---:|\n'
     for suffix,anchor in anchors:
-        # time.sleep(1000)
+        time.sleep(100)
         print('watching:',anchor)
         try:
             r = requests.get(huya_url + suffix,timeout=3)
             html = r.content.decode('utf-8')
             title = re.findall(r'<h1 id="J_roomTitle">(.+)</h1>',html)[0]
-            status = re.findall(r'<span class="host-prevStartTime"><i></i><span>(.+)</span></span>',html)
-            fans = re.findall(r'<div id="activityCount">(\d+)</div>',html)[0]
-            last_live = '直播中'
+            status = re.findall(r'id="live-count">(.+?)</em></span>',html)
+            fans = re.findall(r'id="activityCount">(\d+)</div>',html)[0]
+            last_live = '未直播'
             if status and status[0]:
                 last_live = status[0]
         except:
